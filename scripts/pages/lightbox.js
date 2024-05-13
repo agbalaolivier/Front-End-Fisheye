6d90bc9e3ed65c16c7ce713e5d950bc2d4672eff
Olivier Agbala <ronniking99@gmail.com>
2024-05-14 01:50:20 +0200

    lightbox et tri

--- scripts/pages/lightbox.js
+++ scripts/pages/lightbox.js
@@ -1,62 +1,84 @@
 export default class Lightbox {
     constructor() {
         this.lightbox = document.createElement('div');
-
-        if (!this.lightbox) {
-            // Crée la lightbox si elle n'existe pas
-            this.lightbox = document.createElement('div');
             this.lightbox.id = 'lightbox';
             document.body.appendChild(this.lightbox);
-        }
-        
-    console.log("this.lightbox");
+            document.addEventListener('keydown', this.handleKeydown);
 
-        
+         // Création des éléments média de la lightbox
 
         this.lightboxMedia = document.createElement('img');
-        console.log(this.lightboxMedia)
         this.lightboxMedia.id = 'lightbox-media';
         this.lightbox.appendChild(this.lightboxMedia);
 
+         // Création du bouton de fermeture
+
         this.closeButton = document.createElement('button');
-        this.closeButton.textContent = 'Close';
         this.closeButton.classList.add('close-button');
+        this.closeButton.innerHTML = '<img src="assets/icons/close.svg" />';
         this.lightbox.appendChild(this.closeButton);
 
+         // Création du bouton précédent
+
         this.prevButton = document.createElement('button');
-        this.prevButton.textContent = 'Prev';
-        this.prevButton.id = 'prev-button';
+        this.prevButton.classList.add ('prev-button');
+        this.prevButton.innerHTML = '<i class="fas fa-chevron-left"></i>';
         this.lightbox.appendChild(this.prevButton);
 
+        // Création du bouton suivant
+
         this.nextButton = document.createElement('button');
-        this.nextButton.textContent = 'Next';
-        this.nextButton.id = 'next-button';
+        this.nextButton.classList.add ('next-button');
+        this.nextButton.innerHTML = '<i class="fas fa-chevron-right"></i>';
         this.lightbox.appendChild(this.nextButton);
 
+        // Initialisation de l'index courant
+
         this.currentIndex = 0;
+
+        // Ajout des écouteurs d'événements
 
         this.closeButton.addEventListener('click', () => {
             this.closeLightbox();
         });
+        this.prevButton.addEventListener('click', () => {
+            this.navigate(-1);
+        });
+        this.nextButton.addEventListener('click', () => {
+            this.navigate(1);
+        });
+    }
+
+    handleKeydown = (event) => {
+        switch (event.key) {
+            case 'ArrowRight':
+                this.navigate(1);
+                break;
+            case 'ArrowLeft':
+                this.navigate(-1);
+                break;
+            case 'Escape':
+                this.closeLightbox();
+                break;
+        }
     }
 
     openLightbox(mediaURLs, startIndex) {
+        this.mediaURLs = mediaURLs; // Stockage des URLs des médias
         this.currentIndex = startIndex;
         this.lightboxMedia.src = mediaURLs[startIndex];
         this.lightbox.style.display = 'block';
 
-        this.prevButton.addEventListener('click', () => {
-            this.currentIndex = (this.currentIndex - 1 + mediaURLs.length) % mediaURLs.length;
-            this.lightboxMedia.src = mediaURLs[this.currentIndex];
-        });
+        }
+        navigate(direction) {
+            this.currentIndex = (this.currentIndex + direction + this.mediaURLs.length) % this.mediaURLs.length;
+            this.lightboxMedia.src = this.mediaURLs[this.currentIndex];
+        }
 
-        this.nextButton.addEventListener('click', () => {
-            this.currentIndex = (this.currentIndex + 1) % mediaURLs.length;
-            this.lightboxMedia.src = mediaURLs[this.currentIndex];
-        });
+        closeLightbox() {
+            this.lightbox.style.display = 'none';
+            document.removeEventListener('keydown', this.handleKeydown);
+        }
     }
 
-    closeLightbox() {
-        this.lightbox.style.display = 'none';
-    }
-}
+        
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,7 +4,7 @@ import { PhotographerModel } from "../Model/PhotographerModel.js";
 import { PhotographerTemplate } from "../templates/PhotographerTemplate.js";
 import MediaFactory from "../Factory/MediaFactory.js";
 import MediaTemplate from "../templates/MediaTemplate.js";
-import { displayModal, closeModal } from "../utils/contactForm.js";
+import {displayModal , closeModal} from "../utils/contactForm.js";
 import Lightbox from "../pages/lightbox.js";
 
 
@@ -15,7 +15,9 @@ class PhotographerApp {
         this.photographerApi = new PhotographerApi();
         this.mediaApi = new MediaApi();
         this.modal = document.getElementById("contact_modal");
-        this.contactForm = document.querySelector('form'); 
+        this.contactForm = document.querySelector('form');
+        this.initializeFormValidation(); // Ajouter cette ligne pour initialiser la validation 
+        
 
     }
 
@@ -24,6 +26,9 @@ class PhotographerApp {
             const { photographerData, mediaDatas } = await this.getPhotographerData();
             this.displayPhotographer(photographerData, mediaDatas);
             this.initLightbox(); 
+            this.createSortOptions(); // Appeler la méthode pour créer et insérer les options de tri
+            this.sortMedia(criteria)
+
         } catch (error) {
             console.error('An error occurred during initialization:', error);
         }
@@ -115,16 +120,138 @@ initLightbox() {
     const lightbox = new Lightbox();
 
     // Sélectionnez vos médias et créez un tableau d'URL
-    const mediaArray = [...document.querySelectorAll('.media-container img')];
+    const mediaArray = [...document.querySelectorAll('.media-container img,.media-container video')];
     const mediaURLs = mediaArray.map(media => media.src);
 
     // Ajoutez un gestionnaire d'événements à chaque média
     mediaArray.forEach((media, index) => {
+        media.removeEventListener('click', this.handleMediaClick); // Supprimez l'ancien gestionnaire pour éviter les doublons
         media.addEventListener('click', () => {
+            console.log(`Media at index ${index} clicked`, media.src); // Affiche l'URL du média cliqué
             lightbox.openLightbox(mediaURLs, index);
         });
     });
+
+   
 }
+handleMediaClick = (mediaURLs, index) => {
+    console.log(`Media at index ${index} clicked`, mediaURLs[index]); // Affiche l'URL du média cliqué
+    lightbox.openLightbox(mediaURLs, index);
+}
+//verification modale
+initializeFormValidation() {
+    this.contactForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // Empêcher la soumission du formulaire
+        const firstName = this.contactForm.querySelector('[name="first_name"]').value;
+        const lastName = this.contactForm.querySelector('[name="last_name"]').value;
+        const email = this.contactForm.querySelector('[name="email"]').value;
+        const message = this.contactForm.querySelector('[name="message"]').value;
+
+        if (!this.isValidInput(firstName, 2)) {
+            alert('Le nom doit contenir au moins 2 caractères.');
+            return;
+        }
+        if (!this.isValidInput(lastName, 2)) {
+            alert('Le prénom doit contenir au moins 2 caractères.');
+            return;
+        }
+        if (!this.isValidEmail(email)) {
+            alert('L\'email n\'est pas valide.');
+            return;
+        }
+        if (!this.isValidInput(message, 10)) {
+            alert('Le message doit contenir au moins 10 caractères.');
+            return;
+        }
+
+        this.contactForm.submit(); // Soumettre le formulaire si tout est valide
+    });
+}
+
+isValidInput(input, minLength) {
+    return input.length >= minLength;
+}
+
+isValidEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+// option de triage
+createSortOptions() {
+    const mainContainer = document.getElementById('main'); 
+
+    if (!mainContainer) {
+        console.error('Élément main non trouvé');
+        return;
+    }
+
+    
+    
+    const sortOptionsContainer = document.createElement('div');
+    sortOptionsContainer.classList.add('option-container');
+    sortOptionsContainer.textContent = 'Trier par';
+    sortOptionsContainer.addEventListener('click', () => {
+        optionsContainer.style.display = optionsContainer.style.display === 'none' ? 'block' : 'none';
+    });
+    
+    const sortByTitleButton = document.createElement('button');
+    sortByTitleButton.classList.add('sort-by-title');
+    sortByTitleButton.textContent = 'Titre';
+    sortByTitleButton.addEventListener('click', () => this.sortMedia('title')); 
+
+    const sortByPopularityButton = document.createElement('button');
+    sortByPopularityButton.classList.add('sort-by-popularity');
+    sortByPopularityButton.textContent = 'Popularité';
+    sortByPopularityButton.addEventListener('click', () => this.sortMedia('popularity'));
+
+    const optionsContainer = document.createElement('div');
+    optionsContainer.classList.add('options-container');
+    optionsContainer.style.display = 'none'; // Caché par défaut
+
+    optionsContainer.appendChild(sortByTitleButton);
+    optionsContainer.appendChild(sortByPopularityButton);
+
+    sortOptionsContainer.appendChild(optionsContainer);
+
+
+    const photographHeaderElement = document.querySelector('.photograph-header');
+    mainContainer.insertBefore(sortOptionsContainer, photographHeaderElement.nextSibling);
+
+
+}
+sortMedia(criteria) {
+    
+    const mainContainer = document.getElementById('main');
+    const mediaContainers = document.querySelectorAll('.media-container');
+
+    mediaContainers.forEach(container => {
+        const mediaArray = Array.from(container.children);
+        if (criteria === 'title') {
+            mediaArray.sort((a, b) => {
+                const titleA = a.dataset.title.toUpperCase();
+                const titleB = b.dataset.title.toUpperCase();
+                if (titleA < titleB) {
+                    return -1;
+                }
+                if (titleA > titleB) {
+                    return 1;
+                }
+                return 0;
+            });
+        } else if (criteria === 'popularity') {
+            mediaArray.sort((a, b) => {
+                const popularityA = parseInt(a.dataset.popularity);
+                const popularityB = parseInt(b.dataset.popularity);
+                return popularityB - popularityA;
+            });
+        }
+        mediaArray.forEach(media => container.appendChild(media));
+    });
+    
+
+    }
+
 
 
 }
