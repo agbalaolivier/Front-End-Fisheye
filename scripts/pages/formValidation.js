005e0dc6d2b4e1ad47f4ecbc142779535c651d95
Olivier Agbala <ronniking99@gmail.com>
2024-07-04 15:51:28 +0200

    finalisation toujours

--- scripts/pages/formValidation.js
+++ scripts/pages/formValidation.js
@@ -3,45 +3,47 @@ export function initializeFormValidation(contactForm){
     contactForm.addEventListener('submit', (event) => {
         event.preventDefault(); // Empêcher la soumission du formulaire
 
-        const firstName = this.contactForm.querySelector('[name="first_name"]').value;
-        const lastName = this.contactForm.querySelector('[name="last_name"]').value;
-        const email = this.contactForm.querySelector('[name="email"]').value;
-        const message = this.contactForm.querySelector('[name="message"]').value;
+        const fields = {
+            firstName: contactForm.querySelector('[name="first_name"]').value,
+            lastName: contactForm.querySelector('[name="last_name"]').value,
+            email: contactForm.querySelector('[name="email"]').value,
+            message: contactForm.querySelector('[name="message"]').value
+        };
 
-        if (!this.isValidInput(firstName, 2)) {
-            console.error('Le prenom doit contenir au moins 2 caractères.');
-            alert('Le prenom doit contenir au moins 2 caractères.');
-            return;
-        }
-        if (!this.isValidInput(lastName, 2)) {
-            console.error('Le nom doit contenirt au moins 2 caractères.');
-            alert('Le nom doit contenir au moins 2 caractères.');
-            return;
-        }
-        if (!this.isValidEmail(email)) {
-            console.error('L\'email n\'est pas valide.');
-            alert('L\'email n\'est pas valide.');
-            return;
-        }
-        if (!this.isValidInput(message, 10)) {
-            console.error('Le message doit contenir au moins 10 caractères.');
-            alert('Le message doit contenir au moins 10 caractères.');
-            return;
-        }
-        // Validation des champs du formulaire
-        if (this.isValidInput(firstName, 2) && this.isValidInput(lastName, 2) && this.isValidEmail(email) && this.isValidInput(message, 10)) {
-            // Afficher le message de succès
-            const successMessage = document.getElementById('success-message');
-            successMessage.style.display = 'block';
+        const errors = {
+            firstName: 'Le prénom doit contenir au moins 2 caractères.',
+            lastName: 'Le nom doit contenir au moins 2 caractères.',
+            email: 'L\'email n\'est pas valide.',
+            message: 'Le message doit contenir au moins 10 caractères.'
+        };
 
-            // Réinitialiser le formulaire après un court délai
-            setTimeout(() => {
-                this.contactForm.reset();
-                successMessage.style.display = 'none';
-            }, 3000); // Afficher le message de succès pendant 3 secondes
+        for (let key in fields) {
+            if (key === 'email') {
+                if (!isValidEmail(fields[key])) {
+                    console.error(errors[key]);
+                    alert(errors[key]);
+                    return;
+                }
+            } else {
+                let minLength = key === 'message' ? 10 : 2;
+                if (!isValidInput(fields[key], minLength)) {
+                    console.error(errors[key]);
+                    alert(errors[key]);
+                    return;
+                }
+            }
         }
+
+        // Afficher le message de succès
+        const successMessage = document.getElementById('success-message');
+        successMessage.style.display = 'block';
+
+        // Réinitialiser le formulaire après un court délai
+        setTimeout(() => {
+            contactForm.reset();
+            successMessage.style.display = 'none';
+        }, 3000); // Afficher le message de succès pendant 3 secondes
     });
-
 }
 
 function isValidInput(input, minLength) {
